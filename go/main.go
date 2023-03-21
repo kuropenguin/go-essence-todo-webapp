19a6ffd99442de1e13f0b8b69b82e81f3b20dbf0
kuropenguin <kuro1994.t@gmail.com>
2023-03-21 14:45:29 +0900

    add gitignore

--- go/main.go
+++ go/main.go
@@ -3,9 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
+	"github.com/uptrace/bun"
 )
+
+type Todo struct {
+	bun.BaseModel `bun:"table:todos,alias:t"`
+	ID            int64     `bun:"id,pk,autoincrement"`
+	Content       string    `bun:"content,notnull"`
+	Done          bool      `bun:"done"`
+	Until         time.Time `bun:"until,nullzero"`
+	CreatedAt     time.Time
+	UpdateAt      time.Time `bun:",nullzero"`
+	DeletedAt     time.Time `bun:",soft_delete,nullzero"`
+}
 
 func main() {
 	fmt.Println("Hello World")
